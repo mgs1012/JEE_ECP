c6d670ea0f629a7f451de3c5cbbdf43e95752689
mgs1012 <maria.mgs2602@gmail.com>
2015-03-11 12:53:10 +0100

    Add class entities.Voto Add class entities.Tema

---
+++ src/main/java/es/miw/persistencia/models/daos/TemaDao.java
@@ -0,0 +1,9 @@
+package es.miw.persistencia.models.daos;
+
+import es.miw.persistencia.models.entities.Tema;
+
+public interface TemaDao extends GenericDao<Tema, Integer> {
+
+}
+
+
---
+++ src/main/java/es/miw/persistencia/models/daos/VotoDao.java
@@ -0,0 +1,7 @@
+package es.miw.persistencia.models.daos;
+
+import es.miw.persistencia.models.entities.Voto;
+
+public interface VotoDao extends GenericDao<Voto, Integer>{
+
+}
