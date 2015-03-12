89cb4c24465fdef20b918fe9f4de34dfb2f3eb64
Maria <Maria@Maria-PC>
2015-03-12 11:06:54 +0100

    Refactor de las clases JDBC. Clase a refactorizar: VotoDaoJdbc. Clase TemaDAoJdbc To-Do

---
+++ src/main/java/es/miw/persistencia/dao/jdbc/TemaDaoJdbc.java
@@ -0,0 +1,53 @@
+package es.miw.persistencia.dao.jdbc;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import es.miw.persistencia.models.daos.DaoFactory;
+import es.miw.persistencia.models.daos.TemaDao;
+import es.miw.persistencia.models.entities.Tema;
+
+public class TemaDaoJdbc extends GenericDaoJdbc<Tema, Integer> implements TemaDao {
+
+	@Override
+	public void create(Tema entity) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Tema read(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void update(Tema entity) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteById(Integer id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<Tema> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public static String sqlToCreateTable() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	}
--- src/main/java/es/miw/persistencia/dao/jdbc/VotoDaoJdbc.java
+++ src/main/java/es/miw/persistencia/dao/jdbc/VotoDaoJdbc.java
@@ -1,5 +1,81 @@
 package es.miw.persistencia.dao.jdbc;
 
-public class VotoDaoJdbc {
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import es.miw.persistencia.models.daos.VotoDao;
+import es.miw.persistencia.models.entities.Tema;
+import es.miw.persistencia.models.entities.Voto;
+import es.miw.persistencia.models.utils.NivelEstudios;
+
+
+public class VotoDaoJdbc extends GenericDaoJdbc<Voto, Integer> implements VotoDao{
+
+	  private Logger log = LogManager.getLogger(TemaDaoJdbc.class);
+
+	    private Voto create(ResultSet resultSet) {
+	        try {
+	            if (resultSet != null && resultSet.next()) {
+	                return new Voto(resultSet.getString(Voto.IP),
+	                        (NivelEstudios) resultSet.getObject((Voto.NIVELESTUDIOS)),
+	                        resultSet.getInt(Voto.VALORACION));
+	            }
+	        } catch (SQLException e) {
+	            log.error("read: " + e.getMessage());
+	        }
+	        return null;
+	    }
+
+	    private static final String SQL_CREATE_TABLE = "CREATE TABLE %s ( %s VARCHAR(20), "
+	            + "%s VARCHAR(255),%s INT NOT NULL,  PRIMARY KEY (%s))";
+
+	    public static String sqlToCreateTable() {
+	        return String.format(SQL_CREATE_TABLE, Voto.TABLE, Voto.IP,
+	        		Voto.NIVELESTUDIOS, Voto.VALORACION,  Voto.ID);
+	    }
+
+	    private static final String SQL_INSERT = "INSERT INTO %s (%s,%s,%s, %s) VALUES (%d,'%s','%s', %d)";
+
+	    @Override
+	    public void create(Voto voto) {
+	        this.updateSql(String.format(SQL_INSERT, Voto.TABLE, Voto.ID, Voto.IP,
+	        		Voto.NIVELESTUDIOS, Voto.VALORACION, voto.getId(), voto.getIp(), voto.getNivelEstudios(), voto.getValoracion()));
+	    }
+
+	    @Override
+	    public Voto read(Integer id) {
+	        ResultSet resultSet = this.query(String.format(SQL_SELECT_ID, Voto.TABLE, id));
+	        return this.create(resultSet);
+	    }
+
+	    private static final String SQL_UPDATE = "UPDATE %s SET %s='%s', %s='%s', %s='%s WHERE ID=%d";
+
+	    @Override
+	    public void update(Voto voto) {
+	        this.updateSql(String.format(SQL_UPDATE, Voto.TABLE,Voto.IP, voto.getIp(),
+	        		Voto.NIVELESTUDIOS, voto.getNivelEstudios(), Voto.VALORACION,
+	        		voto.getValoracion(), voto.getId()));
+	    }
+
+	    @Override
+	    public void deleteById(Integer id) {
+	        this.updateSql(String.format(SQL_DELETE_ID, Voto.TABLE, id));
+	    }
+
+	    @Override
+	    public List<Voto> findAll() {
+	        List<Voto> list = new ArrayList<Voto>();
+	        ResultSet resultSet = this.query(String.format(SQL_SELECT_ALL, Voto.TABLE));
+	        Voto voto = this.create(resultSet);
+	        while (voto != null) {
+	            list.add(voto);
+	            voto = this.create(resultSet);
+	        }
+	        return list;
+	    }
 }
