b7b52136f77cf02ed8f0a632ded521457a7d3f64
Maria <Maria@Maria-PC>
2015-03-12 10:34:54 +0100

    Estructura JDBC - Refactorizadas clases: - GenericDaoJdbc - DaoJdbcFactory

---
+++ src/main/java/es/miw/persistencia/dao/jdbc/DaoJdbcFactory.java
@@ -0,0 +1,67 @@
+package es.miw.persistencia.dao.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import org.apache.logging.log4j.LogManager;
+
+import es.miw.persistencia.models.daos.DaoFactory;
+import es.miw.persistencia.models.daos.TemaDao;
+import es.miw.persistencia.models.daos.VotoDao;
+import es.miw.persistencia.models.entities.Tema;
+import es.miw.persistencia.models.entities.Voto;
+
+public class DaoJdbcFactory extends DaoFactory {
+    private static final String DRIVER = "com.mysql.jdbc.Driver";
+
+    private static final String URL = "jdbc:mysql://localhost:3306/miwjee";
+
+    private static final String USER = "root";
+
+    private static final String PASS = "";
+
+    private static final String DROP_TABLE = "DROP TABLE IF EXISTS %s";
+
+    private static Connection connection;
+
+    public static Connection getConnection() {
+        try {
+            if (connection == null || !connection.isValid(0)) {
+                Class.forName(DRIVER);
+                connection = DriverManager.getConnection(URL, USER, PASS);
+            }
+        } catch (ClassNotFoundException e) {
+            LogManager.getLogger(DaoJdbcFactory.class).error(
+                    "Problemas con el driver: " + e.getMessage());
+        } catch (SQLException e) {
+            LogManager.getLogger(DaoJdbcFactory.class).error(
+                    "Problemas con la BD: " + e.getMessage());
+        }
+        return connection;
+    }
+
+    public static void dropAndCreateTables() {
+        try {
+            Statement statement = getConnection().createStatement();
+            statement.executeUpdate(String.format(DROP_TABLE, Tema.TABLE));
+            statement.executeUpdate(String.format(DROP_TABLE, Voto.TABLE));
+            statement.executeUpdate(TemaDaoJdbc.sqlToCreateTable());
+            statement.executeUpdate(VotoDaoJdbc.sqlToCreateTable());
+        } catch (SQLException e) {
+            LogManager.getLogger(DaoJdbcFactory.class).error("Drop tables: " + e.getMessage());
+        }
+    }
+
+    @Override
+    public TemaDao getTemaDao() {
+        return new TemaDaoJdbc();
+    }
+
+	@Override
+	public VotoDao getVotoDao() {
+		return new VotoDaoJdbc();
+	}
+
+}
