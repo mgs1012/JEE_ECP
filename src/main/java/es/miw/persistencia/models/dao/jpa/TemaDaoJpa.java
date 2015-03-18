8cbc711c07c3dd7fe3d9bde80ee862c9ad438c4b
Maria <Maria@Maria-PC>
2015-03-18 10:53:00 +0100

    Refactorizando método en TemaDaoJpa. Add method Tema getTemabyTitle(String tituloTema)

--- src/main/java/es/miw/persistencia/models/dao/jpa/TemaDaoJpa.java
+++ src/main/java/es/miw/persistencia/models/dao/jpa/TemaDaoJpa.java
@@ -1,23 +1,66 @@
 package es.miw.persistencia.models.dao.jpa;
 
-import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 
 import es.miw.persistencia.models.daos.TemaDao;
 import es.miw.persistencia.models.entities.Tema;
 
 public class TemaDaoJpa extends GenericDaoJpa<Tema, Integer> implements TemaDao {
-	
-	/*@Id
-    private Integer id;
-	
-    private String titulo;
 
-    private String descripcion;*/
-    
-	
+	/*
+	 * @Id private Integer id;
+	 * 
+	 * private String titulo;
+	 * 
+	 * private String descripcion;
+	 */
+
 	public TemaDaoJpa() {
 		super(Tema.class);
 	}
 
+	@Override
+	public Tema getTemaByName(String tituloTema) {
+		EntityManager entityManager = DaoJpaFactory.getEntityManagerFactory()
+				.createEntityManager();
+		CriteriaBuilder criteria = entityManager.getCriteriaBuilder();
+		CriteriaQuery<Tema> query = criteria.createQuery(Tema.class);
+
+		Root<Tema> root = query.from(Tema.class);
+
+		query.select(root);
+
+		Predicate p1 = criteria.equal(root.get("nombre"), tituloTema);
+		query.where(p1);
+		// Se realiza la query
+		query.select(root).where(p1);
+		System.out.println("Voy a buscar en la columna nombre: " + tituloTema);
+		return entityManager.createQuery(query).getSingleResult();
+	}
+
+	/*
+	 * @Override public void removeTema(String titulo) { EntityManager
+	 * entityManager = DaoJpaFactory.getEntityManagerFactory()
+	 * .createEntityManager(); CriteriaBuilder criteria =
+	 * entityManager.getCriteriaBuilder(); CriteriaQuery<Tema> query =
+	 * criteria.createQuery(Tema.class);
+	 * 
+	 * Root<Tema> root = query.from(Tema.class);
+	 * 
+	 * query.select(root);
+	 * 
+	 * Predicate predicado = criteria.equal(root.get("titulo"), titulo);
+	 * query.where(predicado); query.select(root).where(predicado);
+	 * entityManager.getTransaction().begin();
+	 * entityManager.remove(entityManager.createQuery(query).getSingleResult());
+	 * entityManager.getTransaction().commit(); return;
+	 * 
+	 * }
+	 */
 
 }
--- src/main/java/es/miw/persistencia/models/daos/TemaDao.java
+++ src/main/java/es/miw/persistencia/models/daos/TemaDao.java
@@ -3,6 +3,10 @@ package es.miw.persistencia.models.daos;
 import es.miw.persistencia.models.entities.Tema;
 
 public interface TemaDao extends GenericDao<Tema, Integer> {
+
+	Tema getTemaByName(String tituloTema);
+	
+	//public void removeTema(String titulo);
 		
 
 }
